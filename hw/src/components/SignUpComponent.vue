e78772ee7da980f1d83e38725eaa20a70e957caa
Andrei Tsistjakov <duuuw4@gmail.com>
2023-11-24 17:06:58 +0200

    create a signup page

---
+++ hw/src/components/SignUpComponent.vue
@@ -0,0 +1,143 @@
+<template>
+    <div id="signup-page">
+        <div class="form-container">
+            <h1>Sign Up</h1>
+      <form @submit.prevent="signUp" class="signup-form">
+        <div class="form-group">
+          <label for="email">Email</label>
+          <input type="email" v-model="email" placeholder="Email" required>
+        </div>
+  
+        <div class="form-group">
+          <label for="password">Password</label>
+          <input type="password" v-model="password" placeholder="Password" required>
+        </div>
+  
+        <div v-if="!isPasswordValid" class="password-error">
+          Password is not valid. Please follow the specified conditions.
+          <ul>
+            <li>At least 8 characters and less than 15 characters</li>
+            <li>At least one uppercase alphabet character</li>
+            <li>At least two lowercase alphabet characters</li>
+            <li>At least one numeric value</li>
+            <li>Start with an uppercase alphabet</li>
+            <li>Include the character "_"</li>
+          </ul>
+        </div>
+  
+        <button type="submit" class="signup-button">Sign Up</button>
+      </form>
+    </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: "SignUpComponent",
+    data() {
+      return {
+        email: '',
+        password: '',
+        isPasswordValid: true,
+      };
+    },
+    methods: {
+      signUp() {
+        if (!this.validatePassword()) {
+          this.isPasswordValid = false;
+          return;
+        }
+        this.isPasswordValid = true;
+        console.log('User Data:', {
+          email: this.email,
+          password: this.password,
+        });
+      },
+      validatePassword() {
+        const regex = /^(?=.*[A-Z])(?=.*[a-z].*[a-z])(?=.*\d)(?=.*_)[A-Z].{7,14}$/;
+        return regex.test(this.password);
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+
+.form-container {
+  width: 100%;
+  max-width: 400px;
+  margin: auto;
+  margin-top: 25px;
+  margin-bottom: 25px;
+  padding: 215px 100px;
+  background-color: #1f3453;
+  border-radius: 20px;
+}
+
+h1 {
+    color: #fff;
+    margin-bottom: 20px;
+    background-color: #1f3453;
+}
+.signup-form {
+    max-width: 500px;
+    margin: auto;
+    background-color: #1f3453;
+  }
+
+  .form-group {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    background-color: #1f3453;
+    margin-bottom: 20px;
+  }
+  
+  label {
+    font-size: 18px;
+    font-family: 'Arial', sans-serif;
+    font-weight: bold;
+    margin-right: 10px;
+    display: block;
+    margin-bottom: 5px;
+    color: #fff;
+    background-color: #1f3453;
+    width: 100px;
+}
+  
+  input {
+    width: 70%;
+    padding: 20px;
+    font-size: 18px;
+    background-color: #324e79;
+
+    border-radius: 20px;
+  }
+  
+  .password-error {
+    position: absolute;
+    color: red;
+    margin-top: 100px;
+  }
+  
+  .signup-button {
+    background-color: #38598b;
+    color: #fff;
+    padding: 20px 30px;
+    border: none;
+    border-radius: 20px;
+    cursor: pointer;
+    font-size: 16px;
+  }
+
+  
+  .signup-button:hover {
+    background-color: #113f67;
+  }
+  
+  @media screen and (max-width: 800px){
+    .signup-form {
+      width: 100%;
+    }
+  }
+  </style>
--- hw/src/views/SignupView.vue
+++ hw/src/views/SignupView.vue
@@ -1,5 +1,78 @@
 <template>
-    <div class="signup">
-     <h2> Sign up page</h2>
-    </div>
-   </template>
+  <div class="div">
+        <div class= "sidebar1"></div>
+        <div class="signup">
+            <sign-up-component></sign-up-component> 
+        </div>
+        <div class= "sidebar2"></div>
+  </div>
+</template>
+
+<script>
+
+import SignUpComponent from '@/components/SignUpComponent.vue';
+export default{
+name: "SignUp",
+components: { SignUpComponent},
+data() {
+    return {
+      username: '',
+      email: '',
+      password: '',
+      isPasswordValid: true,
+    };
+  },
+  methods: {
+    signUp() {
+      if (!this.validatePassword()) {
+        this.isPasswordValid = false;
+        return;
+      }
+      this.isPasswordValid = true;
+
+      console.log('User Data:', {
+        username: this.username,
+        email: this.email,
+        password: this.password,
+      });
+    },
+    validatePassword() {
+      const regex = /^(?=.*[A-Z])(?=.*[a-z].*[a-z])(?=.*\d)(?=.*_)[A-Z].{7,14}$/;
+      return regex.test(this.password);
+    },
+  },
+};
+</script>
+
+<style>
+.div {
+  display: flex;
+}
+
+.signup{
+    width:100%;
+   }
+.sidebar1{
+    margin:20px 0px 20px 0px;
+    width:20%;
+    background-color: #38598b;
+    border-radius: 25px;
+}
+.sidebar2{
+    margin:20px 0px 20px 0px;
+    width:20%;
+    background-color: #38598b;
+    border-radius: 25px;
+}
+
+h2 {
+  color: #38598b;
+  font-size: 24px;
+  margin-bottom: 20px;
+}
+
+@media screen and (max-width: 800px){
+    .sidebar1, .sidebar2{width:0px;}
+    .signup{ width:100%}
+}
+</style>
